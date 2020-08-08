3385de3c480433bf2574dad58f0782c8da065e3b
Luca Patera <lucapatera@outlook.it>
2020-08-08 18:55:15 +0200

    Fixed wrong fputcsv logic

--- src/Utils.php
+++ src/Utils.php
@@ -3,28 +3,23 @@
 namespace Lukasss93\Larex;
 
 use Illuminate\Support\Collection;
-use Keboola\Csv\CsvOptions;
-use Keboola\Csv\CsvReader;
-use Keboola\Csv\CsvWriter;
-use Keboola\Csv\Exception;
-use Keboola\Csv\InvalidArgumentException;
+use Illuminate\Support\Str;
 
 class Utils
 {
-    
     /**
      * Get a collection from csv file
      * @param string $filename
      * @return Collection
-     * @throws Exception
      */
     public static function csvToCollection(string $filename): Collection
     {
         $output = collect([]);
-        $reader = new CsvReader($filename, ';', CsvOptions::DEFAULT_ENCLOSURE, CsvOptions::DEFAULT_ESCAPED_BY);
-        foreach($reader as $row) {
-            $output->push($row);
+        $file = fopen($filename, 'rb');
+        while (($columns = fgetcsv($file, 0, ';')) !== false) {
+            $output->push($columns);
         }
+        fclose($file);
         return $output;
     }
     
@@ -32,15 +27,14 @@ class Utils
      * Write a collection to csv file
      * @param Collection $array
      * @param string $filename
-     * @throws Exception
-     * @throws InvalidArgumentException
      */
     public static function collectionToCsv(Collection $array, string $filename): void
     {
-        $writer = new CsvWriter($filename, ';', null, PHP_EOL);
-        foreach($array as $row) {
-            $writer->writeRow($row);
+        $file = fopen($filename, 'wb');
+        foreach ($array as $row) {
+            self::fputcsv($file, $row, ';');
         }
+        fclose($file);
     }
     
     public static function getStub(string $name): string
@@ -56,10 +50,10 @@ class Utils
     
     public static function writeKeyValue($key, $value, &$file, int $level = 1): void
     {
-        if(is_array($value)) {
+        if (is_array($value)) {
             fwrite($file, str_repeat('    ', $level) . "'$key' => [" . PHP_EOL);
             $level++;
-            foreach($value as $childKey => $childValue) {
+            foreach ($value as $childKey => $childValue) {
                 self::writeKeyValue($childKey, $childValue, $file, $level);
             }
             fwrite($file, str_repeat('    ', $level - 1) . "]," . PHP_EOL);
@@ -75,8 +69,8 @@ class Utils
     {
         $env = getenv('NOLOOP');
         
-        if($env !== '1') {
-            while(true) {
+        if ($env !== '1') {
+            while (true) {
                 $callback();
             }
         } else {
@@ -84,4 +78,30 @@ class Utils
         }
     }
     
+    public static function fputcsv($handle, $array, $delimiter = ',', $enclosure = '"', $eol = PHP_EOL)
+    {
+        $output = '';
+        
+        $count = count($array);
+        foreach ($array as $i => $item) {
+            $item = self::normalizeEOLs($item);
+            
+            if (Str::contains($item, $enclosure)) {
+                $item = str_replace($enclosure, "\\" . $enclosure, $item);
+            }
+            
+            if (Str::contains($item, [$delimiter, PHP_EOL])) {
+                $item = $enclosure . $item . $enclosure;
+            }
+            
+            $output .= $item;
+            
+            if ($i < $count - 1) {
+                $output .= $delimiter;
+            }
+        }
+        $output .= $eol;
+        
+        return fwrite($handle, $output);
+    }
 }
