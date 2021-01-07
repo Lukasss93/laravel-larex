1607f43cc29393b19198d9fc00d135fbb1bcb71f
Luca Patera <lucapatera@outlook.it>
2021-01-07 23:46:20 +0100

    Added tests for DuplicateValueLinter

---
+++ tests/Linters/DuplicateValueLinterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Lukasss93\Larex\Tests\Linters;
+
+use Lukasss93\Larex\Console\LarexLintCommand;
+use Lukasss93\Larex\Linters\DuplicateValueLinter;
+use Lukasss93\Larex\Tests\TestCase;
+
+class DuplicateValueLinterTest extends TestCase
+{
+    public function test_successful(): void
+    {
+        config(['larex.linters' => [
+            DuplicateValueLinter::class,
+        ]]);
+        
+        $this->initFromStub('linters/duplicate-value/success');
+        
+        $this->artisan(LarexLintCommand::class)
+            ->expectsOutput('OK (1 linter)')
+            ->run();
+    }
+    
+    public function test_failure(): void
+    {
+        config(['larex.linters' => [
+            DuplicateValueLinter::class,
+        ]]);
+        
+        $this->initFromStub('linters/duplicate-value/failure');
+        
+        $this->artisan(LarexLintCommand::class)
+            ->expectsOutput(' FAIL  1 duplicate value found:')
+            ->expectsOutput('└ row 3 (app.ark), columns: 3 (en), 4 (it)')
+            ->expectsOutput('FAILURES!')
+            ->expectsOutput('Linters: 1, Failures: 1')
+            ->run();
+    }
+}
