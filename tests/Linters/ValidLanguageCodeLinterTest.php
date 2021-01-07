473f5346f7699461fb834f6b68ce79cb5661b558
Luca Patera <lucapatera@outlook.it>
2021-01-07 02:04:24 +0100

    Added tests for ValidLanguageCodeLinter

---
+++ tests/Linters/ValidLanguageCodeLinterTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Lukasss93\Larex\Tests\Linters;
+
+use Lukasss93\Larex\Console\LarexLintCommand;
+use Lukasss93\Larex\Linters\ValidLanguageCodeLinter;
+use Lukasss93\Larex\Tests\TestCase;
+
+class ValidLanguageCodeLinterTest extends TestCase
+{
+    public function test_successful(): void
+    {
+        config(['larex.linters' => [
+            ValidLanguageCodeLinter::class,
+        ]]);
+    
+        $this->initFromStub('linters/valid-language-code/success');
+        
+        $this->artisan(LarexLintCommand::class)
+            ->expectsOutput('OK (1 linter)')
+            ->run();
+    }
+    
+    public function test_failure(): void
+    {
+        config(['larex.linters' => [
+            ValidLanguageCodeLinter::class,
+        ]]);
+        
+        $this->initFromStub('linters/valid-language-code/failure');
+        
+        $this->artisan(LarexLintCommand::class)
+            ->expectsOutput(' FAIL  Language code not valid in column 3 (xxx).')
+            ->expectsOutput('FAILURES!')
+            ->expectsOutput('Linters: 1, Failures: 1')
+            ->run();
+    }
+    
+    public function test_failure_and_suggest(): void
+    {
+        config(['larex.linters' => [
+            ValidLanguageCodeLinter::class,
+        ]]);
+        
+        $this->initFromStub('linters/valid-language-code/failure-and-suggest');
+        
+        $this->artisan(LarexLintCommand::class)
+            ->expectsOutput(' FAIL  Language code not valid in column 3 (it_IF). Did you mean: it_IT')
+            ->expectsOutput('FAILURES!')
+            ->expectsOutput('Linters: 1, Failures: 1')
+            ->run();
+    }
+}
