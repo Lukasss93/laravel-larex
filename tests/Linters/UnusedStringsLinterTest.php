29270cc7b7a76b564512e566dd282b60631b4f03
Luca Patera <lucapatera@outlook.it>
2021-01-08 00:11:26 +0100

    Added tests for UnusedStringsLinter

---
+++ tests/Linters/UnusedStringsLinterTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Lukasss93\Larex\Tests\Linters;
+
+use Illuminate\Support\Facades\File;
+use Lukasss93\Larex\Console\LarexLintCommand;
+use Lukasss93\Larex\Linters\UnusedStringsLinter;
+use Lukasss93\Larex\Tests\TestCase;
+
+class UnusedStringsLinterTest extends TestCase
+{
+    public function test_successful(): void
+    {
+        config(['larex.linters' => [
+            UnusedStringsLinter::class,
+        ]]);
+        
+        $this->initFromStub('linters/unused-strings/success');
+        
+        $this->artisan(LarexLintCommand::class)
+            ->expectsOutput('OK (1 linter)')
+            ->run();
+    }
+    
+    public function test_failure(): void
+    {
+        config(['larex.linters' => [
+            UnusedStringsLinter::class,
+        ]]);
+        
+        $this->initFromStub('linters/unused-strings/failure');
+        
+        $this->artisan(LarexLintCommand::class)
+            ->expectsOutput(' FAIL  1 unused string found:')
+            ->expectsOutput('└ app.apple is unused at line 4')
+            ->expectsOutput('FAILURES!')
+            ->expectsOutput('Linters: 1, Failures: 1')
+            ->run();
+    }
+    
+    /**
+     * @beforeClass
+     */
+    public function beforeAll(): void
+    {
+        //create a test blade file with @lang functions
+        $testFilePath = resource_path('views' . PHP_EOL . 'test.blade.php');
+        
+        if (File::exists($testFilePath)) {
+            File::delete($testFilePath);
+        }
+        
+        File::put($testFilePath, $this->getTestStub('linters/unused-strings/blade'));
+    }
+}
