37aca89eed7bc058568d5843d4b64946948958a8
Luca Patera <lucapatera@outlook.it>
2021-04-08 02:26:39 +0200

    Added CsvReader

---
+++ src/Support/CsvReader.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Lukasss93\Larex\Support;
+
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\LazyCollection;
+use Spatie\SimpleExcel\SimpleExcelReader;
+
+class CsvReader
+{
+    private SimpleExcelReader $reader;
+
+    protected function __construct(string $path)
+    {
+        $this->reader = SimpleExcelReader::create($path);
+    }
+
+    public static function create(string $path): self
+    {
+        return new self($path);
+    }
+
+    public function getHeader(): Collection
+    {
+        $header = $this->reader->getHeaders();
+        return is_array($header) ? collect($header) : collect([]);
+    }
+
+    public function getRows(): LazyCollection
+    {
+        return $this->reader->getRows()->mapInto(Collection::class);
+    }
+}
