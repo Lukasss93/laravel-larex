bdb1ec472175cbbbbf7f2fc8a546a3f3a0883cbf
Luca Patera <lucapatera@outlook.it>
2021-08-11 00:38:37 +0200

    Installed pest

---
+++ tests/Pest.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Test Case
+|--------------------------------------------------------------------------
+|
+| The closure you provide to your test functions is always bound to a specific PHPUnit test
+| case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
+| need to change it using the "uses()" function to bind a different classes or traits.
+|
+*/
+
+use Illuminate\Support\Facades\File;
+use Lukasss93\Larex\Support\Utils;
+use Lukasss93\Larex\Tests\TestCase;
+
+uses(TestCase::class)
+    ->beforeEach(function () {
+        //set global csv settings
+        config([
+            'larex.csv' => [
+                'path' => 'resources/lang/localization.csv',
+            ],
+            'larex.search' => [
+                'dirs' => ['resources/views'],
+                'patterns' => ['*.php'],
+                'functions' => ['__', 'trans', '@lang'],
+            ],
+        ]);
+
+        //clear lang folder
+        $items = glob(resource_path('lang/*'));
+        foreach ($items as $item) {
+            if (is_dir($item)) {
+                File::deleteDirectory($item);
+            } else {
+                File::delete($item);
+            }
+        }
+    })
+    ->in(__DIR__);
+
+/*
+|--------------------------------------------------------------------------
+| Expectations
+|--------------------------------------------------------------------------
+|
+| When you're writing tests, you often need to check that values meet certain conditions. The
+| "expect()" function gives you access to a set of "expectations" methods that you can use
+| to assert different things. Of course, you may extend the Expectation API at any time.
+|
+*/
+
+expect()->extend('fileContent', fn () => $this->and(File::get($this->value)));
+expect()->extend('toEqualStub', fn ($name) => $this->toEqual(getTestStub($name)));
+
+/*
+|--------------------------------------------------------------------------
+| Functions
+|--------------------------------------------------------------------------
+|
+| While Pest is very powerful out-of-the-box, you may have some testing code specific to your
+| project that you don't want to repeat in every file. Here you can also expose helpers as
+| global functions to help you to reduce the number of lines of code in your test files.
+|
+*/
+
+function localizationPath($relative = false): string
+{
+    $path = 'resources/lang/localization.csv';
+
+    if ($relative) {
+        return $path;
+    }
+
+    return base_path($path);
+}
+
+function getTestStub(string $name, $eol = "\n"): string
+{
+    $name = str_replace('.', '/', $name);
+    $content = file_get_contents(__DIR__.'/Stubs/'.$name.'.stub');
+
+    return Utils::normalizeEOLs($content, $eol);
+}
+
+function initFromStub(string $stub): void
+{
+    File::put(localizationPath(), getTestStub($stub));
+}
