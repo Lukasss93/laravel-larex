a82b6ccfba0a79cc5dc64c72f702f83fd1920ce4
Luca Patera <lucapatera@outlook.it>
2022-11-30 20:52:53 +0100

    Comments refactoring inside the config file

--- src/config/larex.php
+++ src/config/larex.php
@@ -4,40 +4,52 @@ return [
 
     /*
      |--------------------------------------------------------------------------
-     | Larex Settings
+     | Default CSV settings
      |--------------------------------------------------------------------------
+     |
+     | Here you can specify the default settings for CSV files.
+     |
      */
 
-    /**
-     * Default CSV settings.
-     */
     'csv' => [
         'path' => lang_path('localization.csv'),
     ],
 
-    /**
-     * Exporters.
+    /*
+     |--------------------------------------------------------------------------
+     | Larex Exporters
+     |--------------------------------------------------------------------------
+     |
+     | Here you can specify the exporters that will be used by larex:export command.
+     | You can add your own exporters (they must implement the \Lukasss93\Larex\Contracts\Exporter interface) 
+     | by adding them to the "list" array.
+     | Calling the "larex:export" command without the exporter parameter will use the "default" exporter.
+     |
      */
+    
     'exporters' => [
-        // Default exporter
         'default' => 'laravel',
-
-        // Available exporters
         'list' => [
             'laravel' => Lukasss93\Larex\Exporters\LaravelExporter::class,
             'json:lang' => Lukasss93\Larex\Exporters\JsonLanguagesExporter::class,
             'json:group' => Lukasss93\Larex\Exporters\JsonGroupsExporter::class,
         ],
     ],
-
-    /**
-     * Importers.
+    
+    /*
+     |--------------------------------------------------------------------------
+     | Larex Importers
+     |--------------------------------------------------------------------------
+     |
+     | Here you can specify the importers that will be used by "larex:import" command.
+     | You can add your own importers (they must implement the \Lukasss93\Larex\Contracts\Importer interface) 
+     | by adding them to the "list" array.
+     | Calling the "larex:import" command without the importer parameter will use the "default" importer.
+     |
      */
-    'importers' => [
-        // Default importer
-        'default' => 'laravel',
 
-        // Available importers
+    'importers' => [
+        'default' => 'laravel',
         'list' => [
             'laravel' => Lukasss93\Larex\Importers\LaravelImporter::class,
             'json:lang' => Lukasss93\Larex\Importers\JsonLanguagesImporter::class,
@@ -45,9 +57,19 @@ return [
         ],
     ],
 
-    /**
-     * Linters to run with larex:lint command.
+    /*
+     |--------------------------------------------------------------------------
+     | Linters
+     |--------------------------------------------------------------------------
+     |
+     | Linters to run with "larex:lint" command.
+     | Linters are executed in the order they are defined.
+     | You can disable a linter by commenting it out.
+     | You can add your own linters (they must implement the \Lukasss93\Larex\Contracts\Linter interface) 
+     | by adding them to the list.
+     |
      */
+    
     'linters' => [
         Lukasss93\Larex\Linters\ValidHeaderLinter::class,
         Lukasss93\Larex\Linters\ValidLanguageCodeLinter::class,
@@ -60,42 +82,58 @@ return [
         // Lukasss93\Larex\Linters\ValidHtmlValueLinter::class,
         // Lukasss93\Larex\Linters\SameParametersLinter::class,
     ],
-
-    /**
-     * Used by SameParametersLinter.
+    
+    /*
+     |--------------------------------------------------------------------------
+     | Ignore Empty Values
+     |--------------------------------------------------------------------------
+     |
+     | If true, empty values will be ignored when running the "SameParametersLinter" linter.
+     |
      */
+
     'ignore_empty_values' => false,
-
-    /**
-     * Search criteria for file used by:
-     * - UntranslatedStringsLinter
-     * - UnusedStringsLinter
-     * - LarexLocalizeCommand.
+    
+    /*
+     |--------------------------------------------------------------------------
+     | Search Criteria
+     |--------------------------------------------------------------------------
+     |
+     | Here you can specify the search criteria that will be used by:
+     | - the "larex:localize" command
+     | - the "UntranslatedStringsLinter" linter
+     | - the "UnusedStringsLinter" linter
+     |
+     | The "dirs" array contains the directories to search for strings.
+     | Note: it's recursive.
+     |
+     | The "patterns" array contains the patterns to search for strings.
+     | The values can be a regular expression, glob, or just a string.
+     |
+     | The "functions" array contains the functions that the strings will be extracted from.
+     | Note: The translation string should always be the first argument.
+     |
      */
+
     'search' => [
-        /**
-         * Directories which should be looked inside.
-         * NOTE: It's recursive.
-         */
         'dirs' => ['resources/views'],
-
-        /**
-         * Patterns by which files should be queried.
-         * The values can be a regular expression, glob, or just a string.
-         */
         'patterns' => ['*.php'],
-
-        /**
-         * Functions that the strings will be extracted from.
-         * Add here any custom defined functions.
-         * NOTE: The translation string should always be the first argument.
-         */
         'functions' => ['__', 'trans', '@lang'],
     ],
-
-    /**
-     * End of line used by:
-     * - LaravelExporter.
+    
+    /*
+     |--------------------------------------------------------------------------
+     | EOL
+     |--------------------------------------------------------------------------
+     |
+     | End of line character used by "larex:export" command.
+     | EOL can be one of the following values:
+     | - PHP_EOL (default, based on the operating system)
+     | - "\r\n" (Windows)
+     | - "\n" (Unix, Mac OS X)
+     | - "\r" (Mac OS 9 and before)
+     | 
      */
+
     'eol' => PHP_EOL,
 ];
